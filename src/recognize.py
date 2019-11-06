8897b581ea5118b4d5cf232d6743eaa5c3a5e0b5
grepto <grepto@gmail.com>
2019-11-06 17:29:39 +0300

    fixs for flake8 compliance

--- src/recognize.py
+++ src/recognize.py
@@ -1,8 +1,8 @@
+from asyncio import TimeoutError
+
 from envparse import env
 from google.cloud import speech
-from google.cloud.speech import enums
-from google.cloud.speech import types
-from asyncio import TimeoutError
+from google.cloud.speech import enums, types
 
 env.read_envfile()
 
@@ -10,12 +10,12 @@ env.read_envfile()
 def do_recognition(stream: bytes) -> list:
     client = speech.SpeechClient()
 
-    audio = types.RecognitionAudio(
-        content=stream)
+    audio = types.RecognitionAudio(content=stream)
     config = types.RecognitionConfig(
         encoding=enums.RecognitionConfig.AudioEncoding.OGG_OPUS,
         sample_rate_hertz=16000,
-        language_code='ru-RU')
+        language_code='ru-RU',
+    )
     try:
         recognition = client.long_running_recognize(config, audio).result(timeout=90)
         return [result.alternatives[0].transcript for result in recognition.results]
@@ -25,6 +25,4 @@ def do_recognition(stream: bytes) -> list:
 
 def recognize(stream: bytes) -> str:
     recognized = do_recognition(stream)
-    for element in recognized:
-        print(element)
     return ' '.join(recognized)
