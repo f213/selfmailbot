b7c29d5f8c99c818b7b8020ed485d755853d9d84
grepto <grepto@gmail.com>
2019-11-15 12:52:12 +0300

    add blank lime befor assert in test_send_short_voice_recognized

--- tests/test_send_voice.py
+++ tests/test_send_voice.py
@@ -95,6 +95,7 @@ def test_send_short_voice_recognized(bot_app, update, send_mail, recognition_res
     recognition_result.return_value = ['большой', 'зеленый камнеед', 'сидит', 'в пруду']
     bot_app.call('send_voice', update)
     attachment = send_mail.call_args[1]['attachment']
+
     send_mail.assert_called_once_with(
         user_id=update.message.from_user.id,
         to='mocked@test.org',
