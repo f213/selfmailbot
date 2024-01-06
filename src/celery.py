8e597debb2d1bf5ed0ba4acf1ad8483395a0a470
Fedor Borshev <fedor@borshev.com>
2024-01-06 10:45:21 +0300

    Dropped voice funcionality

--- src/celery.py
+++ src/celery.py
@@ -3,25 +3,23 @@ from celery import Celery
 from envparse import env
 from sentry_sdk.integrations.celery import CeleryIntegration
 
-from .helpers import get_subject
 from .mail import send_mail
 from .models import User
-from .recognize import recognize
 from .tpl import get_template
 
 env.read_envfile()
 
-celery = Celery('app')
+celery = Celery("app")
 
 celery.conf.update(
-    broker_url=env('CELERY_BROKER_URL'),
-    task_always_eager=env('CELERY_ALWAYS_EAGER', cast=bool, default=False),
-    task_serializer='pickle',  # we transfer binary data like photos or voice messages,
-    accept_content=['pickle'],
+    broker_url=env("CELERY_BROKER_URL"),
+    task_always_eager=env("CELERY_ALWAYS_EAGER", cast=bool, default=False),
+    task_serializer="pickle",  # we transfer binary data like photos or voice messages,
+    accept_content=["pickle"],
 )
 
-if env('SENTRY_DSN', default=None) is not None:
-    sentry_sdk.init(env('SENTRY_DSN'), integrations=[CeleryIntegration()])
+if env("SENTRY_DSN", default=None) is not None:
+    sentry_sdk.init(env("SENTRY_DSN"), integrations=[CeleryIntegration()])
 
 
 @celery.task
@@ -29,8 +27,8 @@ def send_confirmation_mail(user_id):
     user = User.get(User.pk == user_id)
     send_mail(
         to=user.email,
-        subject='[Selfmailbot] Confirm your email',
-        text=get_template('email/confirmation.txt').render(user=user),
+        subject="[Selfmailbot] Confirm your email",
+        text=get_template("email/confirmation.txt").render(user=user),
     )
 
 
@@ -46,11 +44,11 @@ def send_text(user_id, subject, text):
 
 
 @celery.task
-def send_file(user_id, file, filename, subject, text=''):
+def send_file(user_id, file, filename, subject, text=""):
     user = User.get(User.pk == user_id)
 
     if not text:
-        text = ' '
+        text = " "
 
     send_mail(
         to=user.email,
@@ -58,22 +56,4 @@ def send_file(user_id, file, filename, subject, text=''):
         subject=subject,
         attachment=file,
         attachment_name=filename,
-    )
-
-
-@celery.task
-def send_recognized_voice(user_id, file, duration):
-    if duration <= 60:
-        recognized_text = recognize(file.read())
-        subject = 'Voice: {}'.format(get_subject(recognized_text)) if recognized_text else 'Voice note to self'
-    else:
-        recognized_text = ''
-        subject = 'Voice note to self'
-
-    send_file(
-        user_id=user_id,
-        file=file,
-        filename='voice.oga',
-        subject=subject,
-        text=recognized_text,
     )
