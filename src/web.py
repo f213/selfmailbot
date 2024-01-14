ae0a3db669da967e3155983a38adfd07a4f7b1ef
Fedor Borshev <fedor@borshev.com>
2024-01-14 19:35:16 +0300

    Confirmation webapp domain change

--- src/web.py
+++ src/web.py
@@ -14,7 +14,14 @@ app = Flask("confirmation_webapp", template_folder="src/templates/html")
 init_sentry()
 
 
-@app.route("/confirm/<key>/")
+@app.route("/healthcheck/")
+def healthcheck() -> str:
+    with kombu.Connection(os.getenv("CELERY_BROKER_URL")) as connection:
+        connection.connect()
+        return render_template("ok.html")
+
+
+@app.route("/<uuid:key>/")
 def confirm(key: str) -> str:
     user = get_user_by_confirmation_link(key)
 
@@ -25,10 +32,3 @@ def confirm(key: str) -> str:
     user.save()
 
     return render_template("confirmation_ok.html")
-
-
-@app.route("/healthcheck/")
-def healthcheck() -> str:
-    with kombu.Connection(os.getenv("CELERY_BROKER_URL")) as connection:
-        connection.connect()
-        return render_template("ok.html")
